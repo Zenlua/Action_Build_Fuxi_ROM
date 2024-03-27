5d6f1cc8970538f852678d9cc43e50a179b32130
险梦 <84411809+00XM01@users.noreply.github.com>
2024-03-27 21:53:10 +0800

    change

--- make.sh
+++ make.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 URL="$1"              # 移植包下载地址
-VENDOR_URL="$2"       # 底包下载地址
-GITHUB_ENV="$3"       # 输出环境变量
-GITHUB_WORKSPACE="$4" # 工作目录
+GITHUB_ENV="$2"       # 输出环境变量
+GITHUB_WORKSPACE="$3" # 工作目录
 
-device=houji # 设备代号
+device=fuxi # 设备代号
 
-Red='\033[1;31m'    # 粗体红色
-Yellow='\033[1;33m' # 粗体黄色
-Blue='\033[1;34m'   # 粗体蓝色
-Green='\033[1;32m'  # 粗体绿色
-
-port_os_version=$(echo ${URL} | cut -d"/" -f4)                   # 移植包的 OS 版本号, 例: OS1.0.7.0.UNACNXM
-port_version=$(echo ${port_os_version} | sed 's/OS1/V816/g')     # 移植包的实际版本号, 例: V816.0.7.0.UNACNXM
-port_zip_name=$(echo ${URL} | cut -d"/" -f5)                     # 移植包的 zip 名称, 例: miui_AURORA_OS1.0.7.0.UNACNXM_81a48e3c20_14.0.zip
-vendor_os_version=$(echo ${VENDOR_URL} | cut -d"/" -f4)          # 底包的 OS 版本号, 例: OS1.0.32.0.UNCCNXM
-vendor_version=$(echo ${vendor_os_version} | sed 's/OS1/V816/g') # 底包的实际版本号, 例: V816.0.32.0.UNCCNXM
-vendor_zip_name=$(echo ${VENDOR_URL} | cut -d"/" -f5)            # 底包的 zip 名称, 例: miui_HOUJI_OS1.0.32.0.UNCCNXM_4fd0e15877_14.0.zip
-
+zip_name=$(echo ${URL} | cut -d"/" -f5)        #包名，例：miui_FUXI_OS1.0.24.3.18.DEV_e208767273_14.0.zip
+os_version=$(echo ${URL} | cut -d"/" -f4)      #版本号，例：OS1.0.24.3.18.DEV  
 android_version=$(echo ${URL} | cut -d"_" -f5 | cut -d"." -f1) # Android 版本号, 例: 14
 build_time=$(date) && build_utc=$(date -d "$build_time" +%s)   # 构建时间
 
@@ -37,6 +26,11 @@ erofs_extract="$GITHUB_WORKSPACE"/tools/extract.erofs
 erofs_mkfs="$GITHUB_WORKSPACE"/tools/mkfs.erofs
 lpmake="$GITHUB_WORKSPACE"/tools/lpmake
 apktool_jar="java -jar "$GITHUB_WORKSPACE"/tools/apktool.jar"
+
+Red='\033[1;31m'    # 粗体红色
+Yellow='\033[1;33m' # 粗体黄色
+Blue='\033[1;34m'   # 粗体蓝色
+Green='\033[1;32m'  # 粗体绿色
 
 Start_Time() {
   Start_ns=$(date +'%s%N')
@@ -79,56 +73,54 @@ End_Time() {
 
 ### 系统包下载
 echo -e "${Red}- 开始下载系统包"
-echo -e "${Yellow}- 开始下载移植包"
 Start_Time
-aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$GITHUB_WORKSPACE" "$URL"
-End_Time 下载移植包
-Start_Time
-echo -e "${Yellow}- 开始下载底包"
-aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$GITHUB_WORKSPACE" "$VENDOR_URL"
-End_Time 下载底包
+if [ ! -f "$GITHUB_WORKSPACE/${zip_name}" ]; then
+    # 系统包不存在，则执行下载
+    aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$GITHUB_WORKSPACE" "$URL"
+else
+    echo "系统包已存在，无需下载"
+fi
+End_Time 下载系统包
 ### 系统包下载结束
 
 ### 解包
-echo -e "${Red}- 开始解压系统包"
-mkdir -p "$GITHUB_WORKSPACE"/Third_Party
-mkdir -p "$GITHUB_WORKSPACE"/"${device}"
-mkdir -p "$GITHUB_WORKSPACE"/images/config
+echo -e "${Red}- 准备解包"
+mkdir -p "$GITHUB_WORKSPACE"/Extra_dir
+mkdir -p "$GITHUB_WORKSPACE"/"${device}" #底包payload.bin存放目录
+mkdir -p "$GITHUB_WORKSPACE"/images/config #移植包product,system,system_ext存放目录（images）
 mkdir -p "$GITHUB_WORKSPACE"/zip
 
-echo -e "${Yellow}- 开始解压移植包"
+echo -e "${Yellow}- 开始解包"
 Start_Time
-$a7z x "$GITHUB_WORKSPACE"/$port_zip_name -r -o"$GITHUB_WORKSPACE"/Third_Party >/dev/null
-rm -rf "$GITHUB_WORKSPACE"/$port_zip_name
-End_Time 解压移植包
-echo -e "${Yellow}- 开始解压底包"
-Start_Time
-$a7z x "$GITHUB_WORKSPACE"/${vendor_zip_name} -o"$GITHUB_WORKSPACE"/"${device}" payload.bin >/dev/null
-rm -rf "$GITHUB_WORKSPACE"/${vendor_zip_name}
-End_Time 解压底包
-mkdir -p "$GITHUB_WORKSPACE"/Extra_dir
-echo -e "${Red}- 开始解底包payload"
-$payload_extract -s -o "$GITHUB_WORKSPACE"/Extra_dir/ -i "$GITHUB_WORKSPACE"/"${device}"/payload.bin -X system,system_ext,product -e -T0
-sudo rm -rf "$GITHUB_WORKSPACE"/"${device}"/payload.bin
-echo -e "${Red}- 开始分解底包image"
+$a7z x "$GITHUB_WORKSPACE"/${zip_name} -o"$GITHUB_WORKSPACE"/"${device}" payload.bin >/dev/null
+echo -e "${Red}- 开始解payload"
+$payload_extract -s -o "$GITHUB_WORKSPACE"/Extra_dir/ -i "$GITHUB_WORKSPACE"/"${device}"/payload.bin -x -T0
+for i in product system system_ext; do
+  echo -e "${Red}- 开始转移product system system_ext"
+  sudo mv -f "$GITHUB_WORKSPACE"/Extra_dir/$i.img "$GITHUB_WORKSPACE"/images/
+done
+
+echo -e "${Red}- 转移完成，开始删除原包"
+rm -rf "$GITHUB_WORKSPACE"/${zip_name} #删除原包
+End_Time 
+
+echo -e "${Red}- 开始第一次分解image"
 for i in mi_ext odm system_dlkm vendor vendor_dlkm; do
-  echo -e "${Yellow}- 正在分解底包: $i.img"
+  echo -e "${Yellow}- 正在分解image: $i.img"
   cd "$GITHUB_WORKSPACE"/"${device}"
   sudo $erofs_extract -s -i "$GITHUB_WORKSPACE"/Extra_dir/$i.img -x
   rm -rf "$GITHUB_WORKSPACE"/Extra_dir/$i.img
 done
+
 sudo mkdir -p "$GITHUB_WORKSPACE"/"${device}"/firmware-update/
-sudo cp -rf "$GITHUB_WORKSPACE"/Extra_dir/* "$GITHUB_WORKSPACE"/"${device}"/firmware-update/
+sudo cp -rf "$GITHUB_WORKSPACE"/Extra_dir/* "$GITHUB_WORKSPACE"/"${device}"/firmware-update/ #底包mi_ext odm system_dlkm vendor vendor_dlkm存放目录
 cd "$GITHUB_WORKSPACE"/images
-echo -e "${Red}- 开始解移植包payload"
-$payload_extract -s -o "$GITHUB_WORKSPACE"/images/ -i "$GITHUB_WORKSPACE"/Third_Party/payload.bin -X product,system,system_ext -T0
-echo -e "${Red}- 开始分解移植包image"
+echo -e "${Red}- 开始第二次分解image"
 for i in product system system_ext; do
-  echo -e "${Yellow}- 正在分解移植包: $i"
+  echo -e "${Yellow}- 正在分解: $i"
   sudo $erofs_extract -s -i "$GITHUB_WORKSPACE"/images/$i.img -x
   rm -rf "$GITHUB_WORKSPACE"/images/$i.img
 done
-sudo rm -rf "$GITHUB_WORKSPACE"/Third_Party
 ### 解包结束
 
 ### 写入变量
@@ -136,121 +128,49 @@ echo -e "${Red}- 开始写入变量"
 # 构建日期
 echo "build_time=$build_time" >>$GITHUB_ENV
 echo -e "${Blue}- 构建日期: $build_time"
-# 移植包版本
-echo "port_os_version=$port_os_version" >>$GITHUB_ENV
-echo -e "${Blue}- 移植包版本: $port_os_version"
-# 底包版本
-echo "vendor_os_version=$vendor_os_version" >>$GITHUB_ENV
-echo -e "${Blue}- 底包版本: $vendor_os_version"
-# 移植包安全补丁
+# 包版本
+echo "os_version=$os_version" >>$GITHUB_ENV
+echo -e "${Blue}- 移植包版本: $os_version"
+# 包安全补丁
 system_build_prop=$(find "$GITHUB_WORKSPACE"/images/system/system/ -maxdepth 1 -type f -name "build.prop" | head -n 1)
-port_security_patch=$(grep "ro.build.version.security_patch=" "$system_build_prop" | awk -F "=" '{print $2}')
-echo -e "${Blue}- 移植包安全补丁版本: $port_security_patch"
-echo "port_security_patch=$port_security_patch" >>$GITHUB_ENV
-# 底包安全补丁
-vendor_build_prop=$GITHUB_WORKSPACE/${device}/vendor/build.prop
-vendor_security_patch=$(grep "ro.vendor.build.security_patch=" "$vendor_build_prop" | awk -F "=" '{print $2}')
-echo -e "${Blue}- 底包安全补丁版本: $vendor_security_patch"
-echo "vendor_security_patch=$vendor_security_patch" >>$GITHUB_ENV
-# 移植包基线版本
-port_base_line=$(grep "ro.system.build.id=" "$system_build_prop" | awk -F "=" '{print $2}')
-echo -e "${Blue}- 移植包基线版本: $port_base_line"
-echo "port_base_line=$port_base_line" >>$GITHUB_ENV
-# 底包基线版本
-vendor_base_line=$(grep "ro.vendor.build.id=" "$vendor_build_prop" | awk -F "=" '{print $2}')
-echo -e "${Blue}- 底包基线版本: $vendor_base_line"
-echo "vendor_base_line=$vendor_base_line" >>$GITHUB_ENV
+security_patch=$(grep "ro.build.version.security_patch=" "$system_build_prop" | awk -F "=" '{print $2}')
+echo -e "${Blue}- 包安全补丁版本: $security_patch"
+echo "security_patch=$security_patch" >>$GITHUB_ENV
+# 包基线版本
+base_line=$(grep "ro.system.build.id=" "$system_build_prop" | awk -F "=" '{print $2}')
+echo -e "${Blue}- 包基线版本: $base_line"
+echo "port_base_line=$base_line" >>$GITHUB_ENV
 ### 写入变量结束
 
 ### 功能修复
 echo -e "${Red}- 开始功能修复"
 Start_Time
+
 # 添加 KernelSU 支持 (可选择)
 echo -e "${Red}- 添加 KernelSU 支持 (可选择)"
 mkdir -p "$GITHUB_WORKSPACE"/init_boot
 cd "$GITHUB_WORKSPACE"/init_boot
-cp -f "$GITHUB_WORKSPACE"/"${device}"/firmware-update/init_boot.img "$GITHUB_WORKSPACE"/init_boot
-$ksud boot-patch -b "$GITHUB_WORKSPACE"/init_boot/init_boot.img --magiskboot $magiskboot --kmi android14-6.1
+cp -f "$GITHUB_WORKSPACE"/"${device}"/firmware-update/init_boot.img "$GITHUB_WORKSPACE"/init_boot/
+$ksud boot-patch -b "$GITHUB_WORKSPACE"/init_boot/init_boot.img --magiskboot $magiskboot --kmi android14-5.15
 mv -f "$GITHUB_WORKSPACE"/init_boot/kernelsu_boot*.img "$GITHUB_WORKSPACE"/"${device}"/firmware-update/init_boot-kernelsu.img
 rm -rf "$GITHUB_WORKSPACE"/init_boot
-# 替换 vendor_boot 的 fstab
-echo -e "${Red}- 替换 Vendor Boot 的 fstab"
-mkdir -p "$GITHUB_WORKSPACE"/vendor_boot
-cd "$GITHUB_WORKSPACE"/vendor_boot
-mv -f "$GITHUB_WORKSPACE"/"${device}"/firmware-update/vendor_boot.img "$GITHUB_WORKSPACE"/vendor_boot
-$magiskboot unpack -h "$GITHUB_WORKSPACE"/vendor_boot/vendor_boot.img 2>&1
-if [ -f ramdisk.cpio ]; then
-  comp=$($magiskboot decompress ramdisk.cpio 2>&1 | grep -v 'raw' | sed -n 's;.*\[\(.*\)\];\1;p')
-  if [ "$comp" ]; then
-    mv -f ramdisk.cpio ramdisk.cpio.$comp
-    $magiskboot decompress ramdisk.cpio.$comp ramdisk.cpio 2>&1
-    if [ $? != 0 ] && $comp --help 2>/dev/null; then
-      $comp -dc ramdisk.cpio.$comp >ramdisk.cpio
-    fi
-  fi
-  mkdir -p ramdisk
-  chmod 755 ramdisk
-  cd ramdisk
-  EXTRACT_UNSAFE_SYMLINKS=1 cpio -d -F ../ramdisk.cpio -i 2>&1
-fi
-sudo cp -f "$GITHUB_WORKSPACE"/"${device}"_files/fstab.qcom "$GITHUB_WORKSPACE"/vendor_boot/ramdisk/first_stage_ramdisk/fstab.qcom
-sudo chmod 644 "$GITHUB_WORKSPACE"/vendor_boot/ramdisk/first_stage_ramdisk/fstab.qcom
-cd "$GITHUB_WORKSPACE"/vendor_boot/ramdisk/
-find | sed 1d | cpio -H newc -R 0:0 -o -F ../ramdisk_new.cpio
-cd ..
-if [ "$comp" ]; then
-  $magiskboot compress=$comp ramdisk_new.cpio 2>&1
-  if [ $? != 0 ] && $comp --help 2>/dev/null; then
-    $comp -9c ramdisk_new.cpio >ramdisk.cpio.$comp
-  fi
-fi
-ramdisk=$(ls ramdisk_new.cpio* 2>/dev/null | tail -n1)
-if [ "$ramdisk" ]; then
-  cp -f $ramdisk ramdisk.cpio
-  case $comp in
-  cpio) nocompflag="-n" ;;
-  esac
-  $magiskboot repack $nocompflag "$GITHUB_WORKSPACE"/vendor_boot/vendor_boot.img "$GITHUB_WORKSPACE"/"${device}"/firmware-update/vendor_boot.img 2>&1
-fi
-sudo rm -rf "$GITHUB_WORKSPACE"/vendor_boot
-# 替换 vendor 的 fstab
-echo -e "${Red}- 替换 vendor 的 fstab"
-sudo cp -f "$GITHUB_WORKSPACE"/"${device}"_files/fstab.qcom "$GITHUB_WORKSPACE"/"${device}"/vendor/etc/fstab.qcom
-# 替换 Product 的叠加层
-echo -e "${Red}- 替换 product 的叠加层"
-sudo rm -rf "$GITHUB_WORKSPACE"/images/product/overlay/*
-sudo unzip -o -q "$GITHUB_WORKSPACE"/"${device}"_files/overlay.zip -d "$GITHUB_WORKSPACE"/images/product/overlay
-# 替换 device_features 文件
-echo -e "${Red}- 替换 device_features 文件"
-sudo rm -rf "$GITHUB_WORKSPACE"/images/product/etc/device_features/*
-sudo unzip -o -q "$GITHUB_WORKSPACE"/"${device}"_files/device_features.zip -d "$GITHUB_WORKSPACE"/images/product/etc/device_features/
-# 替换 displayconfig 文件
-echo -e "${Red}- 替换 displayconfig 文件"
-sudo rm -rf "$GITHUB_WORKSPACE"/images/product/etc/displayconfig/*
-sudo unzip -o -q "$GITHUB_WORKSPACE"/"${device}"_files/displayconfig.zip -d "$GITHUB_WORKSPACE"/images/product/etc/displayconfig/
-# 修复精准电量 (亮屏可用时长)
-echo -e "${Red}- 修复精准电量 (亮屏可用时长)"
-sudo rm -rf "$GITHUB_WORKSPACE"/images/system/system/app/PowerKeeper/*
-sudo unzip -o -q "$GITHUB_WORKSPACE"/"${device}"_files/PowerKeeper.zip -d "$GITHUB_WORKSPACE"/images/system/system/app/PowerKeeper/
+
 # 统一 build.prop
 echo -e "${Red}- 统一 build.prop"
-sudo sed -i 's/ro.build.user=[^*]*/ro.build.user=YuKongA/' "$GITHUB_WORKSPACE"/images/system/system/build.prop
+sudo sed -i 's/ro.build.user=[^*]*/ro.build.user=Prume/' "$GITHUB_WORKSPACE"/images/system/system/build.prop
 for port_build_prop in $(sudo find "$GITHUB_WORKSPACE"/images/ -type f -name "build.prop"); do
   sudo sed -i 's/build.date=[^*]*/build.date='"$build_time"'/' "$port_build_prop"
   sudo sed -i 's/build.date.utc=[^*]*/build.date.utc='"$build_utc"'/' "$port_build_prop"
-  sudo sed -i 's/'"${port_os_version}"'/'"${vendor_os_version}"'/g' "$port_build_prop"
-  sudo sed -i 's/'"${port_version}"'/'"${vendor_version}"'/g' "$port_build_prop"
-  sudo sed -i 's/'"${port_base_line}"'/'"${vendor_base_line}"'/g' "$port_build_prop"
   sudo sed -i 's/persist.device_config.mglru_native.lru_gen_config=[^*]*/persist.device_config.mglru_native.lru_gen_config=all/' "$port_build_prop"
 done
 for vendor_build_prop in $(sudo find "$GITHUB_WORKSPACE"/"${device}"/ -type f -name "*build.prop"); do
   sudo sed -i 's/build.date=[^*]*/build.date='"$build_time"'/' "$vendor_build_prop"
   sudo sed -i 's/build.date.utc=[^*]*/build.date.utc='"$build_utc"'/' "$vendor_build_prop"
-  sudo sed -i 's/ro.mi.os.version.incremental=[^*]*/ro.mi.os.version.incremental='"$port_os_version"'/' "$vendor_build_prop"
+  sudo sed -i 's/ro.mi.os.version.incremental=[^*]*/ro.mi.os.version.incremental='"$os_version"'/' "$vendor_build_prop"
 done
-# 精简部分应用
+# 精简部分应用（product分区）
 echo -e "${Red}- 精简部分应用"
-apps=("MIGalleryLockscreen" "MIUIDriveMode" "MIUIDuokanReader" "MIUIGameCenter" "MIUINewHome" "MIUIYoupin" "MIUIHuanJi" "MIUIMiDrive" "MIUIVirtualSim" "ThirdAppAssistant" "XMRemoteController" "MIUIVipAccount" "MiuiScanner" "Xinre" "SmartHome" "MiShop" "MiRadio" "MIUICompass" "MediaEditor" "BaiduIME" "iflytek.inputmethod" "MIService" "MIUIEmail" "MIUIVideo" "MIUIMusicT")
+apps=("MIGalleryLockscreen" "MIUIDriveMode" "MIUIDuokanReader" "MIUIGameCenter" "MIUINewHomeMIUI15" "MIUINewHome" "MIUIYoupin" "MIUIHuanJi" "MIUIMiDrive" "MIUIVirtualSim" "ThirdAppAssistant" "XMRemoteController" "MIUIVipAccount" "MiuiScanner" "Xinre" "SmartHome" "MiShop" "MiRadio" "MIUICompass" "MediaEditor" "BaiduIME" "iflytek.inputmethod" "MIService" "MIUIEmail" "MIUIVideo" "MIUIMusicT")
 for app in "${apps[@]}"; do
   appsui=$(sudo find "$GITHUB_WORKSPACE"/images/product/data-app/ -type d -iname "*${app}*")
   if [[ -n $appsui ]]; then
@@ -258,22 +178,12 @@ for app in "${apps[@]}"; do
     sudo rm -rf "$appsui"
   fi
 done
-# 分辨率修改
-echo -e "${Red}- 分辨率修改"
-sudo sed -i 's/persist.miui.density_v2=[^*]*/persist.miui.density_v2=480/' "$GITHUB_WORKSPACE"/images/product/etc/build.prop
-# 替换相机
-echo -e "${Red}- 替换相机"
-sudo rm -rf "$GITHUB_WORKSPACE"/images/product/priv-app/MiuiCamera/*
-sudo cat "$GITHUB_WORKSPACE"/"${device}"_files/MiuiCamera.apk.1 "$GITHUB_WORKSPACE"/"${device}"_files/MiuiCamera.apk.2 "$GITHUB_WORKSPACE"/"${device}"_files/MiuiCamera.apk.3 >"$GITHUB_WORKSPACE"/"${device}"_files/MiuiCamera.apk
-sudo cp -f "$GITHUB_WORKSPACE"/"${device}"_files/MiuiCamera.apk "$GITHUB_WORKSPACE"/images/product/priv-app/MiuiCamera/
-# 替换相机标定
-echo -e "${Red}- 替换相机标定"
-sudo unzip -o -q "$GITHUB_WORKSPACE"/"${device}"_files/CameraTools_beta.zip -d "$GITHUB_WORKSPACE"/images/product/app/
-# 占位广告应用
+
+# 占位广告应用（product分区）
 echo -e "${Red}- 占位广告应用"
 sudo rm -rf "$GITHUB_WORKSPACE"/images/product/app/MSA/*
 sudo cp -f "$GITHUB_WORKSPACE"/"${device}"_files/MSA.apk "$GITHUB_WORKSPACE"/images/product/app/MSA
-# 替换完美图标
+# 替换完美图标（product分区）
 echo -e "${Red}- 替换完美图标"
 cd "$GITHUB_WORKSPACE"
 git clone https://github.com/pzcn/Perfect-Icons-Completion-Project.git icons --depth 1
@@ -296,20 +206,31 @@ zip -qr "$GITHUB_WORKSPACE"/images/product/media/theme/default/dynamicicons.zip 
 mv "$GITHUB_WORKSPACE"/images/product/media/theme/default/icons.zip "$GITHUB_WORKSPACE"/images/product/media/theme/default/icons
 mv "$GITHUB_WORKSPACE"/images/product/media/theme/default/dynamicicons.zip "$GITHUB_WORKSPACE"/images/product/media/theme/default/dynamicicons
 rm -rf "$GITHUB_WORKSPACE"/icons
-# 常规修改
+
+#去除avb2.0校验
+for i in  vbmeta.img vbmeta_system.img; do
+  echo -e "${Red}- 正在去 "$i" avb2.0校验"
+  sudo $vbmeta "$GITHUB_WORKSPACE"/Extra_dir/"$i"
+done
+
+# 常规修改（vendor分区）
 echo -e "${Red}- 常规修改"
 sudo rm -rf "$GITHUB_WORKSPACE"/"${device}"/vendor/recovery-from-boot.p
 sudo rm -rf "$GITHUB_WORKSPACE"/"${device}"/vendor/bin/install-recovery.sh
-# 修复 init 崩溃
+
+# 修复 init 崩溃（vendor分区）
 echo -e "${Red}- 修复 init 崩溃"
 sudo sed -i "/start qti-testscripts/d" "$GITHUB_WORKSPACE"/"${device}"/vendor/etc/init/hw/init.qcom.rc
-# 内置 TWRP
+
+# 内置 TWRP （解压twrp到${device}"/firmware-update/）
 echo -e "${Red}- 内置 TWRP"
 sudo unzip -o -q "$GITHUB_WORKSPACE"/"${device}"_files/recovery.zip -d "$GITHUB_WORKSPACE"/"${device}"/firmware-update/
+
 # 添加刷机脚本
 echo -e "${Red}- 添加刷机脚本"
 sudo unzip -o -q "$GITHUB_WORKSPACE"/tools/flashtools.zip -d "$GITHUB_WORKSPACE"/images
-# 移除 Android 签名校验
+
+# 移除 Android 签名校验（system分区）
 sudo mkdir -p "$GITHUB_WORKSPACE"/apk/
 echo -e "${Red}- 移除 Android 签名校验"
 sudo cp -rf "$GITHUB_WORKSPACE"/images/system/system/framework/services.jar "$GITHUB_WORKSPACE"/apk/services.apk
@@ -327,9 +248,10 @@ done
 cd "$GITHUB_WORKSPACE"/apk/services/
 sudo $apktool_jar b -q -f -c "$GITHUB_WORKSPACE"/apk/services/ -o services.jar
 sudo cp -rf "$GITHUB_WORKSPACE"/apk/services/services.jar "$GITHUB_WORKSPACE"/images/system/system/framework/services.jar
+
 # 替换更改文件/删除多余文件
 echo -e "${Red}- 替换更改文件/删除多余文件"
-sudo cp -r "$GITHUB_WORKSPACE"/"${device}"/* "$GITHUB_WORKSPACE"/images
+sudo cp -r "$GITHUB_WORKSPACE"/"${device}"/* "$GITHUB_WORKSPACE"/images #（将设备 ${device} 目录下的所有文件和文件夹，包括子目录中的内容，复制到 $GITHUB_WORKSPACE/images 目录中。）
 sudo rm -rf "$GITHUB_WORKSPACE"/"${device}"
 sudo rm -rf "$GITHUB_WORKSPACE"/"${device}"_files
 End_Time 功能修复
@@ -365,15 +287,15 @@ End_Time 压缩super.zst
 # 生成卡刷包
 echo -e "${Red}- 生成卡刷包"
 Start_Time
-sudo $a7z a "$GITHUB_WORKSPACE"/zip/miui_${device}_${port_os_version}.zip "$GITHUB_WORKSPACE"/images/* >/dev/null
+sudo $a7z a "$GITHUB_WORKSPACE"/zip/miui_${device}_${os_version}.zip "$GITHUB_WORKSPACE"/images/* >/dev/null
 sudo rm -rf "$GITHUB_WORKSPACE"/images
 End_Time 压缩卡刷包
 # 定制 ROM 包名
 echo -e "${Red}- 定制 ROM 包名"
-md5=$(md5sum "$GITHUB_WORKSPACE"/zip/miui_${device}_${port_os_version}.zip)
+md5=$(md5sum "$GITHUB_WORKSPACE"/zip/miui_${device}_${os_version}.zip)
 echo "MD5=${md5:0:32}" >>$GITHUB_ENV
 zip_md5=${md5:0:10}
-rom_name="miui_HOUJI_${port_os_version}_${zip_md5}_${android_version}.0_YuKongA.zip"
-sudo mv "$GITHUB_WORKSPACE"/zip/miui_${device}_${port_os_version}.zip "$GITHUB_WORKSPACE"/zip/"${rom_name}"
+rom_name="miui_FUXI_${os_version}_${zip_md5}_${android_version}.0_Prume.zip"
+sudo mv "$GITHUB_WORKSPACE"/zip/miui_${device}_${os_version}.zip "$GITHUB_WORKSPACE"/zip/"${rom_name}"
 echo "rom_name=$rom_name" >>$GITHUB_ENV
 ### 输出卡刷包结束
